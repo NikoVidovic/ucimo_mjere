c6f866da98697444fffe98d072a505e159bde741
NikoVidovic <niko171202@gmail.com>
2024-03-14 01:08:30 +0100

    Dodan input na igri

--- lib/game.dart
+++ lib/game.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'app_bar.dart';
-import 'game_body.dart';
+import 'bottom_app_bar.dart';
 
 class Game extends StatelessWidget {
   const Game({super.key}); //Učimo mjere, 100
@@ -8,6 +8,44 @@ class Game extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
         appBar: AppBarCustom(title: 'Učimo mjere', height: 100.0),
-        body: GameBody());
+        bottomNavigationBar: BottomAppBarCustom(),
+        body: AnswerInput());
+  }
+}
+
+class AnswerInput extends StatefulWidget {
+  const AnswerInput({super.key});
+
+  @override
+  State<AnswerInput> createState() => _AnswerInputState();
+}
+
+class _AnswerInputState extends State<AnswerInput> {
+  final controller = TextEditingController();
+  String text = "";
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void changeText(text) {
+    setState(() {
+      this.text = text;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      TextField(
+        controller: controller,
+        keyboardType: TextInputType.number,
+        decoration: const InputDecoration(labelText: 'Unesite rješenje'),
+        onChanged: (text) => changeText(text),
+      ),
+      Text(text)
+    ]);
   }
 }
