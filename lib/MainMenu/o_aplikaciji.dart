67bc6c03c1f80307dd20da9b6d0b43d2c0a752b4
NikoVidovic <148124329+NikoVidovic@users.noreply.github.com>
2024-06-01 14:24:16 +0200

    O aplikaciji updated

--- lib/MainMenu/o_aplikaciji.dart
+++ lib/MainMenu/o_aplikaciji.dart
@@ -57,14 +57,15 @@ class _OAplikacijiState extends State<OAplikaciji> {
                 'projekta Učimo matematiku. Partneri su Hrvatska zajednica za Down sindrom, Fakultet elektrotehnike i računarstva Sveučilišta u Zagrebu i O.Š. Trnjanska.\n'
                 '\nNajnovija verzija aplikacije razvijena je 2024. u okviru završnog rada na Fakultetu elektrotehnike i računarstva Sveučilišta u Zagrebu u suradnji '
                 's Edukacijsko-rehabilitacijskim fakultetom u Zagrebu. Ovu verziju aplikacije razvio je student Niko Vidović. Završni rad mentorirala je prof. dr. sc. Željka Car uz asistenticu dr. '
-                'sc. Mateu Žilak.\n'
-                '\nOstale aplikacije pronađite na stranicama kompetencijske mreže ICT-AAC (www.ict-aac.ht).\n'
-                '\nPratite nas na društvenim mrežama: facebook.com/ictaac\n'
+                'sc. Mateu Žilak. Stručna suradnica: Ivana Vinceković, univ. mag. rehab. educ.\n'
+                '\nOstale aplikacije pronađite na stranicama kompetencijske mreže ICT-AAC (www.ict-aac.hr).\n'
+                '\nPratite nas na društvenim mrežama: facebook.com/ictaac'
+                '\nUpoznajte se s politikom privatnosti naših aplikacija: http://www.ict-aac.hr/index.php/hr/politika-privatnosti\n'
                 '\nSva prava pridržana.',
                 style: TextStyle(
                     fontSize: appState.fontSize == 1
                         ? screenHeight / 38
-                        : screenHeight / 38 * (appState.fontSize - 0.3),
+                        : screenHeight / 38 * (appState.fontSize - 0.4),
                     color: appState.fontColor)),
           )
         ])));
